22a73b5db4c5390f2e681dd2b44158ba488454a5
cpamidas <avanturestico@gmail.com>
2025-08-26 16:18:17 +0300

    Remove token logic handler

--- commits.sh
+++ commits.sh
@@ -2,7 +2,7 @@ touch log.txt
 
 generate_commit_message() {
   verbs=("Fix" "Add" "Improve" "Update" "Refactor" "Remove" "Optimize")
-  objects=("RPC handler" "CLI argument" "ENS lookup" "token logic" "address parser" "logging" "balance fetch" "validator check" "output format" "retry logic")
+  objects=("RPC handler" "CLI argument" "ENS lookup" "token logic" "address parser" "logging" "balance fetch")
   contexts=("flow" "code" "support" "case" "handler" "logic")
 
   verb=${verbs[$RANDOM % ${#verbs[@]}]}
@@ -12,12 +12,31 @@ generate_commit_message() {
   echo "$verb $object $context"
 }
 
-for i in {1..10}; do
-  echo "entry $i" >> log.txt                       
-  git add .                                       
-  export GIT_AUTHOR_DATE="$(date -d "$((RANDOM % 100 + 1)) days ago" '+%Y-%m-%dT12:00:00')" 
-  export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"   
-  git commit -m "$(generate_commit_message)"     
-  git push origin main                            
-  sleep $((RANDOM % 16 + 15))                     
+N=10  # число коммитов
+
+for i in $(seq 1 $N); do
+  echo "entry $i" >> log.txt
+  git add .
+
+  # равномерное распределение по году
+  base_days=$(( (365 / N) * i ))
+  random_shift=$((RANDOM % 10 - 5))   # случайный сдвиг ±5 дней
+  days=$((base_days + random_shift))
+  if [ $days -lt 1 ]; then days=1; fi
+  if [ $days -gt 365 ]; then days=365; fi
+
+  # случайное время суток
+  hour=$((RANDOM % 24))
+  minute=$((RANDOM % 60))
+  second=$((RANDOM % 60))
+
+  export GIT_AUTHOR_DATE="$(date -d "$days days ago" "+%Y-%m-%dT%H:%M:%S")"
+  export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"
+
+  git commit -m "$(generate_commit_message)"
+  git push origin main
+
+  # задержка между 4 и 8 минутами
+  sleep $((RANDOM % 241 + 240))
 done
+
